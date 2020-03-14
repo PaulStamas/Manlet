7bd64eb3407714c56e2b3567e7f58b987d5076eb
Paul Stamas <pzstamas@gmail.com>
2020-03-14 10:38:46 -0700

    v0.0.1

---
+++ Manlet v0.0.1/AnimatedSprite.gd
@@ -0,0 +1,17 @@
+extends AnimatedSprite
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	
+	pass # Replace with function body.
+
+func _process(delta):
+	speed_scale = abs(get_parent().velocity.x)/400
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
