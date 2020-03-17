7e9a2e7f000c2fc36c99ef610c33bf3097d12b17
Paul Stamas <pzstamas@gmail.com>
2020-03-16 20:47:31 -0700

    Improved rope swing and started tileing

--- Manlet v0.0.1/Scripts/Player.gd
+++ Manlet v0.0.1/Scripts/Player.gd
@@ -18,7 +18,6 @@ const BAD_WALL_JUMP_MULT = 0.75
 
 onready var player_sprite = $AnimatedSprite
 
-#player variables
 var wall_jump_dir = null
 var last_wall_jump_dir = null
 var wall_touch_delta = 0;
@@ -33,9 +32,10 @@ var input_left
 var input_right
 var input_down 
 var input_shoot
+var input_restart
 
 #Hook shot variables
-const MAX_ROPE_LENGTH = 250
+const MAX_ROPE_LENGTH = 350
 const MIN_ROPE_LENGTH = 75
 const ROPE_STRETCH = 5
 const FIRE_SPEED = 1000
@@ -44,30 +44,39 @@ var grapple_point
 var rope_point
 var rope_angle_velocity
 var rope_angle
-var rope_length 
+var rope_length
 onready var hook_shot = load("res://Scenes/HookShot.tscn")
 var rope_point_accel
+var hook_fire = true
 	
+var change_state
+
 func _ready():
 	$"/root/Global".register_player(self)
 	
 func _physics_process(delta):
 	get_inputs()
 	move(delta)
-	rope_swing()
+	hook_shot()
 	if state == "Swing":
 		velocity = Vector2(0,0)
 	move_and_slide(velocity, Vector2.UP)
 	animate()
+	print_change_state()
 
 func get_inputs():
-	input_up = Input.is_action_pressed("ui_up")
+	input_up = Input.is_action_just_pressed("ui_up")
 	input_wall_jump = Input.is_action_just_pressed("ui_up")
 	input_left = Input.is_action_pressed("ui_left")
 	input_right = Input.is_action_pressed("ui_right")
 	input_down = Input.is_action_pressed("ui_down")
 	
 	input_shoot = Input.is_action_just_pressed("ui_shoot")
+	
+	input_restart = Input.is_action_just_pressed("ui_restart")
+	
+	if input_restart:
+		position = Vector2(73, 567)
 	
 func move(delta):
 	
@@ -77,20 +86,16 @@ func move(delta):
 	on_wall()
 	
 	if is_on_floor():
+		rope_init = true
 		velocity.y = 0
 		last_wall_jump_dir = "none";
 		if input_up:
 			velocity.y = -PLAYER_JUMP_VEL
-		if state == "Swing":
-			rope_angle_velocity = 0
+		hook_fire = true
 			
 	velocity.y += GRAVITY;
 	if input_down:
 		velocity.y += FORCE_FALL
-	
-	if state == "swing":
-		velocity.x = 0
-		velocity.y = 0
 
 	if is_on_ceiling():
 		if input_up:
@@ -104,17 +109,27 @@ func move(delta):
 		velocity.x -= PLAYER_SPEED
 		player_sprite.flip_h = true
 		player_dir = "left"
-		state = "Running"
 	elif input_right and !input_left:
 		velocity.x += PLAYER_SPEED
 		player_sprite.flip_h = false
 		player_dir = "right"
-		state = "Running"
 	else:
 		if abs(velocity.x) < PLAYER_MAX_STOP_SPEED:
 			velocity.x = 0
-		state = "Idle"
-	
+		state = "Idle"	
+		
+	if !is_on_floor():
+		if hook_shot.get_class() == "Area2D":
+#			print(hook_shot.attached)
+			if hook_shot.attached and velocity.y >= 0:
+				state = "Swing"
+				hook_fire = false
+				
+	if input_left or input_right:
+		if state != "Swing":
+			state = "Running"
+
+				
 	if velocity.x > PLAYER_MAX_SPEED:
 		velocity.x = PLAYER_MAX_SPEED
 	elif velocity.x < -PLAYER_MAX_SPEED:
@@ -122,10 +137,6 @@ func move(delta):
 	
 	# DELETE THIS LATER
 	
-	if position.y > 10000:
-		position.x = 500
-		position.y = 300
-		velocity.y = 0
 	
 func on_wall():
 	if is_on_wall():
@@ -143,7 +154,7 @@ func on_wall():
 				wall_touch_delta = 0
 				wall_jump_dir = "right"
 				if state == "Swing":
-					rope_angle_velocity = 0
+					rope_angle_velocity *= .5
 				elif input_left && velocity.y > 0:
 					velocity.y *= WALL_FRICTION
 
@@ -167,60 +178,66 @@ func on_wall():
 				velocity.y = -PLAYER_JUMP_VEL
 			last_wall_jump_dir = "right"
 	
-func rope_swing():
+func hook_shot():
 	if input_shoot and state != "swing":
-		if hook_shot.get_class() == "PackedScene":
-			hook_shot = hook_shot.instance()
-			get_parent().add_child(hook_shot)
-		hook_shot.init(FIRE_SPEED, player_dir, position, "Show")
+		if hook_fire:
+			if hook_shot.get_class() == "PackedScene":
+				hook_shot = hook_shot.instance()
+				get_parent().add_child(hook_shot)
+			hook_shot.init(FIRE_SPEED, player_dir, position, "Show")
+			
 	if hook_shot.get_class() == "Area2D":
 		if sqrt(pow(hook_shot.position.x - position.x, 2) + pow(hook_shot.position.y - position.y, 2)) > MAX_ROPE_LENGTH:
 			hook_shot.init(0, "none", position, "Hide")
+			state = "Running"
 		if hook_shot.attached:
-			
-			if !is_on_floor():
-				if rope_init:
-					grapple_point = hook_shot.position
-					rope_point = position
-					rope_angle_velocity = 0
-					rope_length = sqrt(pow(grapple_point.x - position.x, 2) + pow(grapple_point.y - position.y, 2))
-					print(rope_length)
-					rope_init = false
-				
-				if rope_length < MIN_ROPE_LENGTH:
-					rope_length += ROPE_STRETCH
-						
-				rope_angle = find_angle(hook_shot.position, position)
-				var rope_angle_accel = 0.2 * cos(deg2rad(rope_angle))
-				
-				rope_angle_velocity += rope_angle_accel
-				rope_angle += rope_angle_velocity
-			
-				rope_angle_velocity *= MIDAIR_FRICTION
-		
-				rope_point.x = grapple_point.x + (rope_length * cos(deg2rad(rope_angle)))
-				rope_point.y = grapple_point.y + (rope_length * sin(deg2rad(rope_angle)))
-					
-				if input_left:
-					rope_point.x -= 5
-					
-				if input_right:
-					rope_point.x += 5
-					
-				state = "Swing"
-				rope_point_accel = Vector2(rope_point.x - position.x, rope_point.y - position.y)
-				position += rope_point_accel
-		
-				if input_up:
-					state = "Running"
-					hook_shot.init(0, "none", position, "Hide")
-					velocity.y = -PLAYER_JUMP_VEL * 0.75
-					if player_dir == "right":
-						velocity.x = abs(rope_angle_velocity) * 2
-					elif player_dir == "left":
-						velocity.x = -abs(rope_angle_velocity) * 2
+			if state == "Swing":
+				swing()
 		else: 
 			rope_init = true
+
+func swing():
+	if rope_init:
+		grapple_point = hook_shot.position
+		rope_point = position
+		rope_angle_velocity = -velocity.x / 150
+		rope_length = sqrt(pow(grapple_point.x - position.x, 2) + pow(grapple_point.y - position.y, 2))
+		rope_init = false
+	
+	if rope_length < MIN_ROPE_LENGTH:
+		rope_length += ROPE_STRETCH
+	
+		
+	if input_left:
+		position.x -= 5
+		
+	if input_right:
+		position.x += 5
+		
+	rope_angle = find_angle(hook_shot.position, position)
+	var rope_angle_accel = 0.2 * cos(deg2rad(rope_angle))
+	
+	rope_angle_velocity += rope_angle_accel
+	rope_angle += rope_angle_velocity
+
+	rope_angle_velocity *= MIDAIR_FRICTION
+
+	rope_point.x = grapple_point.x + (rope_length * cos(deg2rad(rope_angle)))
+	rope_point.y = grapple_point.y + (rope_length * sin(deg2rad(rope_angle)))
+		
+	rope_point_accel = Vector2(rope_point.x - position.x, rope_point.y - position.y)
+	position += rope_point_accel
+	
+	if input_up and state == "Swing":
+		state = "Running"
+		hook_shot.init(0, "none", position, "Hide")
+		hook_shot.attached = false
+		if player_dir == "right":
+			velocity.x = abs(rope_point_accel.x) * 90
+		elif player_dir == "left":
+			velocity.x = -abs(rope_point_accel.x) * 90
+		velocity.y = -abs(rope_point_accel.y) * 75
+		print(velocity)
 	
 func animate():
 	if state == "Idle":
@@ -232,7 +249,7 @@ func animate():
 		player_sprite.play("Running")
 		player_sprite.speed_scale = abs(velocity.x)/400
 		#delete when white space is fixed
-		player_sprite.position = Vector2(0,-21)
+		player_sprite.position = Vector2(0,-22)
 	
 	elif state == "Swing":
 		player_sprite.play("Running")
@@ -251,3 +268,11 @@ func find_angle(point1, point2):
 	while angle > 360:
 		angle -= 360
 	return(angle)
+
+func print_change_state():
+	if state != change_state:
+		print(state)
+		change_state = state
+#	if hook_shot.get_class() == "Area2D":
+#		if hook_shot.attached:
+#			print(hook_shot.attached)
